8d424850bbecd09d9029cc95bd903aec3a863583
Andr077xaau <RD41CE@student.aau.dk>
2025-12-09 11:54:03 +0100

    Add files via upload
    første version der virker fint, skal stadig redigere Arduino fil, så den udregner volumen af vand

---
+++ ArduinoGetSendData.ino
@@ -0,0 +1,75 @@
+#define TRIG_PIN 3
+#define ECHO_PIN 2
+#define PUMPE_PIN 6
+#define VENTIL_PIN 1 //change later
+
+
+int distance;
+long duration;
+
+int dist1;
+int dist2;
+int distUsed;
+int n = 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+   Serial.begin(9600);
+   pinMode(TRIG_PIN, OUTPUT);
+   pinMode(ECHO_PIN, INPUT);
+   pinMode(PUMPE_PIN, OUTPUT);
+   pinMode(VENTIL_PIN, OUTPUT);
+}
+
+
+int getDistance() {
+
+  delay(50);
+  digitalWrite(TRIG_PIN, HIGH);
+  delay(50);
+  digitalWrite(TRIG_PIN, LOW);
+
+  duration = pulseIn(ECHO_PIN, HIGH);
+  return duration * 0.034 / 2; // cm
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int value = analogRead(A5); 
+
+  distance = getDistance();
+
+  //Serial.println(distance);     //print distance from sencor to water
+
+
+  if(value>=500 && n==1){         //function to start pumpe when used by user
+    digitalWrite(PUMPE_PIN, HIGH);
+    dist1 = getDistance();
+
+    n = 0;
+  }else if(value<=500 && n==0){
+    digitalWrite(PUMPE_PIN, LOW);
+    dist2 = getDistance();
+    
+    distUsed = dist2-dist1;        //find difference betveen distance before water been used, and after. can be used to find how much water in liters is used
+
+    Serial.println(distUsed);
+
+    n = 1;
+  }else if(value>=500 && n == 0){
+    digitalWrite(PUMPE_PIN, HIGH);
+  }else if(value<=500 && n == 1){
+    digitalWrite(PUMPE_PIN, LOW);
+  }
+
+  if(distance <=20){
+    digitalWrite(VENTIL_PIN, LOW);
+  }else{
+    digitalWrite(VENTIL_PIN, HIGH);
+  }
+  
+  delay(1000);
+
+}
---
+++ GetSaveData.c
@@ -0,0 +1,80 @@
+#include <windows.h>
+#include <stdio.h>
+#include <time.h>
+
+
+int ID = 1; // Example ID
+char date[20]; // Buffer to hold date string
+
+int main() {
+
+    FILE *fpt;    //file pointer variable
+    fpt = fopen("MyFile.csv", "w+");   //open file in write mode
+
+
+    HANDLE hSerial;
+    unsigned long bytesRead;
+    char buffer[256];
+    char line[256];
+    int idx = 0;
+
+    hSerial = CreateFile(
+        "\\\\.\\COM3",
+        GENERIC_READ,
+        0,
+        NULL,
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL
+    );
+
+    if (hSerial == INVALID_HANDLE_VALUE) {
+        printf("Error opening COM port\n");
+        return 1;
+    }
+
+    DCB dcbSerialParams = {0};
+    dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
+    GetCommState(hSerial, &dcbSerialParams);
+
+    dcbSerialParams.BaudRate = CBR_9600;
+    dcbSerialParams.ByteSize = 8;
+    dcbSerialParams.StopBits = ONESTOPBIT;
+    dcbSerialParams.Parity   = NOPARITY;
+    SetCommState(hSerial, &dcbSerialParams);
+
+    printf("Reading from COM3...\n");
+
+    fprintf(fpt,"ID;date;ammount\n");
+    while (1) {
+        if (ReadFile(hSerial, buffer, sizeof(buffer), &bytesRead, NULL)) {
+            for (unsigned long i = 0; i < bytesRead; i++) {
+                char c = buffer[i];
+
+                if (c == '\n') {  
+                    line[idx] = '\0';
+
+
+                    time_t currentTime; // get current time
+                    time(&currentTime); // get current time
+
+                    char *timeStr = ctime(&currentTime);
+                    timeStr[strcspn(timeStr, "\n")] = 0; // remove newline character
+
+                    printf("message: %s\n", line);
+                    fprintf(fpt, "%d; %s; %s\n", ID, timeStr, line); //check if time works as intended 
+                    ID++;
+
+                    idx = 0;
+                } else {
+                    line[idx] = c;
+                    idx++;
+                    if (idx >= sizeof(line)) idx = 0; // to avoid overflow
+                }
+            }
+        }
+    }
+
+    CloseHandle(hSerial);
+    return 0;
+}
